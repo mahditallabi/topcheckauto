642ee2508870ab585ff173a53cc1642eb0fe2876
IGAyoubArt <ig.ayoub.dev@gmail.com>
2026-08-11 13:02:24 +0100

    add new

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@sanity/client'
 
 export const sanityClient = createClient({
-  projectId: 'p7zev2vb',   // من sanity.config.ts
+  projectId: '015wvhnp',   // من sanity.config.ts
   dataset: 'production',
   apiVersion: '2026-01-23',
   useCdn: false, // 🔥 مهم
