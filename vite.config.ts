f92145e8ca21dae7e0b3c971b20339fb3d5a59b2
IGAyoubArt <ig.ayoub.dev@gmail.com>
2026-08-11 12:44:49 +0100

    add new

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/topcheckauto/",
+  base: "/",
 
   server: {
     host: "::",
